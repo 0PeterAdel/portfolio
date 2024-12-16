054d6ec381b7de068618ff7a8c5a1d7371f77743
Peter Adel <1peteradel@gmail.com>
2024-12-16 22:43:13 +0200

    Create

---
+++ utils/data/personal-data.js
@@ -0,0 +1,23 @@
+export const personalData = {
+  name: "Peter Adel",
+  profile: '/profile.png',
+  designation: "Data Analytics Specialist",
+  description: "My name is PETER ADEL. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: "0peteradel@proton.me",
+  phone: "+201140906414",
+  address: "Egypt , 6-Octoper",
+  github: "https://github.com/0PeterAdel",
+  linkedIn: 'https://www.linkedin.com/in/1peteradel',
+  cybertalants: "https://cybertalents.com/members/H_0/profile",
+  sololearn: "https://www.sololearn.com/en/profile/21778301",
+  hackerRank: "https://www.hackerrank.com/profile/1peteradel",
+  codeforce: "https://codeforces.com/profile/Peter_Adel",
+  // 
+  facebook: 'https://www.facebook.com/',
+  twitter: 'https://twitter.com/',
+  stackOverflow: 'https://stackoverflow.com/users/16840768/',
+  leetcode: "https://leetcode.com//",
+  devUsername: "said7388",
+  
+  resume: "https://drive.google.com/file/d/1Xl8uCfag6XBESeeZAO0MFBFLOZhxZePC/view?usp=sharing"
+}
