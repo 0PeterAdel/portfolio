3632923a52cd25a8c7f06f05ab28725be3a2c07c
Peter Adel <148042680+0PeterAdel@users.noreply.github.com>
2025-05-12 09:22:18 +0300

    Update index.jsx

--- app/components/homepage/projects/index.jsx
+++ app/components/homepage/projects/index.jsx
@@ -5,7 +5,7 @@ import ProjectCard from './project-card';
 
 const Projects = () => {
   return (
-    <div id="projects" className="relative z-50 my-12 lg:my-24">
+    <div id="projects" className="relative my-12 lg:my-24">
       <div className="sticky top-10">
         <div className="w-[80px] h-[80px] bg-violet-100 rounded-full absolute -top-3 left-0 translate-x-1/2 filter blur-3xl opacity-30"></div>
         <div className="flex items-center justify-start relative">
@@ -18,13 +18,12 @@ const Projects = () => {
       <div className="pt-24">
         <div className="flex flex-col gap-6">
           {projectsData.map((project, index) => {
-            // نستخدم index+1 عشان آخر بطاقة (index الأكبر) تكون الأعلى z-index
-            const zIndex = index + 1;
+            const zIndex = 100 + index;
             return (
               <div
                 id={`sticky-card-${project.id}`}
                 key={project.id}
-                className="sticky-card w-full mx-auto max-w-2xl sticky"
+                className="sticky-card w-full mx-auto max-w-2xl sticky top-24"
                 style={{ zIndex }}
               >
                 <div className="box-border flex items-center justify-center rounded shadow-[0_0_30px_0_rgba(0,0,0,0.3)] transition-all duration-[0.5s]">
